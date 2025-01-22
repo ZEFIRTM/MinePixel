b3b890bd20f6e09435d16ad29c5b06b8c477eb8f
ZEFIRTM <168871980+ZEFIRTM@users.noreply.github.com>
2025-01-22 03:07:23 +0300

    Add files via upload
    33

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,28 +1,10 @@
-//#if USE_DATA_CACHING
-// const cacheName = "d4rk_ltd-MinePixel-0.1.21";
-// const contentToCache = [
-//     "Build/2f23d236c53c89a67de904edede8f038.loader.js",
-//     "Build/ddcc48b07ea5017a31867f1ae0bc3a11.framework.js.unityweb",
-//#if USE_THREADS
-//     "Build/",
-//#endif
-//     "Build/2be24f842076c69ee157ec39a92695fe.data.unityweb",
-//     "Build/6855db44b718b9a28b71daa841c4e016.wasm.unityweb",
-//     "TemplateData/style.css"
-// ];
-//#endif
+const cacheName = "d4rk_ltd-MinePixel-0.1.22";
 
 self.addEventListener('install', function (e) {
     console.log('[Service Worker] Install');
+    self.skipWaiting(); // Принудительно активируем новый Service Worker
     
-//#if USE_DATA_CACHING
-    e.waitUntil((async function () {
-      // const cache = await caches.open(cacheName);
-      console.log('[Service Worker] Caching all: app shell and content');
-      // await cache.addAll(contentToCache);
-    })());
-//#endif
-
+    // Очищаем все кеши при установке
     e.waitUntil(
         caches.keys().then((cacheNames) => {
             return Promise.all(
@@ -34,33 +16,21 @@ self.addEventListener('install', function (e) {
     );
 });
 
-//#if USE_DATA_CACHING
 self.addEventListener('fetch', function (e) {
-    e.respondWith((async function () {
-      let response = await caches.match(e.request);
-      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-      if (response) { return response; }
-
-      response = await fetch(e.request);
-      // const cache = await caches.open(cacheName);
-      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-      // cache.put(e.request, response.clone());
-      return response;
-    })());
+    // Всегда получаем свежую версию контента
+    e.respondWith(
+        fetch(e.request, {
+            cache: 'no-store'
+        }).catch(error => {
+            console.error('Fetch error:', error);
+            throw error;
+        })
+    );
 });
-//#endif
 
 self.addEventListener('activate', (event) => {
-    const cacheWhitelist = ['your-new-cache-name']; // Укажите имя нового кеша
     event.waitUntil(
-        caches.keys().then((cacheNames) => {
-            return Promise.all(
-                cacheNames.map((cacheName) => {
-                    if (cacheWhitelist.indexOf(cacheName) === -1) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
+        // Принимаем контроль над всеми клиентами сразу
+        clients.claim()
     );
 });
