d5f5d36af2422b05700dbf627b86f925a904f83c
ZEFIRTM <168871980+ZEFIRTM@users.noreply.github.com>
2025-01-28 19:19:04 +0300

    Add files via upload
    NNF1

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "d4rk_ltd-MinePixel-0.1.33";
+const cacheName = "d4rk_ltd-MinePixel-0.1.34";
 
 self.addEventListener('install', function (e) {
     console.log('[Service Worker] Install');
