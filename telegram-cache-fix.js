ec8f57088e6f6618ac86a9b292ec3af0d902ce57
ZEFIRTM <168871980+ZEFIRTM@users.noreply.github.com>
2026-01-08 04:34:38 +0300

    Add files via upload
    BxA2

--- telegram-cache-fix.js
+++ telegram-cache-fix.js
@@ -4,7 +4,7 @@
 (function() {
     'use strict';
     
-    const VERSION = '0.2.13';
+    const VERSION = '0.2.14';
     const CACHE_PREFIX = 'MinePixel-Cache';
     
     // Функция для принудительной очистки всех кэшей
