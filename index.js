75c761d19bb263c889791c4d63410f2b17b57dc7
ZEFIRTM <168871980+ZEFIRTM@users.noreply.github.com>
2026-02-03 22:08:53 +0300

    Add files via upload
    SMF16

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Регистрируем Service Worker с версией
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('ServiceWorker.js?v=0.2.36')
+    navigator.serviceWorker.register('ServiceWorker.js?v=0.2.37')
         .then(function(registration) {
             console.log('ServiceWorker registration successful with scope: ', registration.scope);
         })
@@ -100,15 +100,15 @@ function unityShowBanner(msg, type)
 }
 
 var buildUrl = "Build";
-var loaderUrl = buildUrl + "/8e984fe962478e09f1f88ce7d8fc9f8e.loader.js?v=0.2.36";
+var loaderUrl = buildUrl + "/28d4438b3b1ad53a27c2a7b16cdb4c78.loader.js?v=0.2.37";
 var config = {
-  dataUrl: buildUrl + "/7c9bfa12f221eac6628fdc50d0780798.data.unityweb?v=0.2.36",
-  frameworkUrl: buildUrl + "/d74c9b022d0dfd0f7013108c174844a6.framework.js.unityweb?v=0.2.36",
-  codeUrl: buildUrl + "/64a258e2dc58999cbef54bbdbd03b710.wasm.unityweb?v=0.2.36",
+  dataUrl: buildUrl + "/aa7adcb39f3ead02f654e7d94979786e.data.unityweb?v=0.2.37",
+  frameworkUrl: buildUrl + "/26914a9100ab9662a1d64c0967f5785b.framework.js.unityweb?v=0.2.37",
+  codeUrl: buildUrl + "/5b32810c8c34c28d3b685a7d805ff31e.wasm.unityweb?v=0.2.37",
   streamingAssetsUrl: "StreamingAssets",
   companyName: "d4rk_ltd",
   productName: "MinePixel",
-  productVersion: "0.2.36",
+  productVersion: "0.2.37",
   showBanner: unityShowBanner,
 };
 
@@ -128,7 +128,7 @@ if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent))
   document.getElementsByTagName('head')[0].appendChild(meta);
 }
 
-canvas.style.background = "url('" + buildUrl + "/bcabcc8a777dfc26c6c98dbdb4fc2755.jpg?v=0.2.36') center / cover";
+canvas.style.background = "url('" + buildUrl + "/bcabcc8a777dfc26c6c98dbdb4fc2755.jpg?v=0.2.37') center / cover";
 loadingBar.style.display = "block";
 
 // Функция для принудительного обновления кэша
@@ -149,7 +149,7 @@ function forceCacheUpdate() {
 
 // Проверяем версию и обновляем кэш при необходимости
 function checkVersionAndUpdate() {
-    const currentVersion = '0.2.36';
+    const currentVersion = '0.2.37';
     const storedVersion = localStorage.getItem('gameVersion');
     
     if (storedVersion !== currentVersion) {
@@ -204,7 +204,7 @@ window.addEventListener('load', function ()
   attachTelegramParamsToUrl();
 
   console.log("Telegram Web App has been expanded to full screen");
-  console.log("Game Version: 0.2.36");
+  console.log("Game Version: 0.2.37");
 
   var version = Telegram.WebApp.version;
   var versionFloat = parseFloat(version);
